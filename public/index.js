c942fb631ec314adf2b0e35d0c925be2298b6fa9
Benjamin Shoemaker <shoebenfilm@gmail.com>
2023-04-15 15:36:14 -0500

    Figured out how to hide credentials

--- public/index.js
+++ public/index.js
@@ -1,13 +1,13 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHWq-XEkd83bo2AEhhcioyM7gTZuwQRPs",
-  authDomain: "blazechathub.firebaseapp.com",
-  databaseURL: "https://blazechathub-default-rtdb.firebaseio.com",
-  projectId: "blazechathub",
-  storageBucket: "blazechathub.appspot.com",
-  messagingSenderId: "227346961795",
-  appId: "1:227346961795:web:1c9b117adf212bb63dd2b6",
-  measurementId: "G-J3PVVPX2BG"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
